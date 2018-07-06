aa1548675f65fb4d2143de4c2d21c05adc661030
Joseph Parker <jparker010190@gmail.com>
2018-07-06 23:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue){
   let result
-  if (someValue < 400)
-    result = "This one is on me!"
-
+  if (someValue < 400){
+    result = "This one is on me!";
+  }
+  return result
 }
