c00f59ffa72d6ef656af5444a0ea609839cfad7e
Joseph Parker <jparker010190@gmail.com>
2018-07-07 00:01:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue){
   if (someValue < 400){
     result = "This one is on me!";
   }
-  else if (someValue > 2000 AND someValue < 2500){
+  else if (someValue > 2000 && someValue < 2500){
     result = "I will gladly take your thirty bucks."
   }
   else if (someValue > 2500){
